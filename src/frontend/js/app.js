4396941abf214dfdde1b8469f3e67334ba2f7343
Alexei Rashevskyi <keepcalm312000@gmail.com>
2021-05-16 17:24:35 +0300

    add app.js

---
+++ src/frontend/js/app.js
@@ -0,0 +1,30 @@
+var login = document.getElementById('login')
+function loginAutorize() {
+    //то что мы будем отправлять
+    var data = {
+        login: login.value.trim(),
+        password: password.value,
+    };
+    //парсим в json
+    var json = JSON.stringify(data);
+    //создаем реквест, у него тут же можно засетить хедеры куки и т д
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'http://localhost:8081/authorization');
+    //контент тайп хедер нужен обьязательно
+    xhr.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+    //отправляем
+    xhr.send(json);
+    //прописываем действия при соединении
+    xhr.onload = () => {
+        //реди стейт не задумываемся - всегда четверку пхаем
+        if (xhr.readyState === 4) {
+            //статус который вам будет в ответ приходить, от него отталкиваемся что нам делать, вытаскиваем данные через xhr.body
+            if (xhr.status === 200) {
+                document.location.href = 'http://localhost:4080/menu.html';
+            } else if (xhr.status === 401) {
+                authError.innerText = xhr.responseText;
+                authError.setAttribute('style', 'display: block;');
+            }
+        }
+    }
+}
