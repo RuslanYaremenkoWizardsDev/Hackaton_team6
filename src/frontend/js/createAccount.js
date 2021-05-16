8ab9a750246a2e5c2473e4aea1debd85f065ece1
sorshireality <polina.doctorwho@gmail.com>
2021-05-16 12:13:28 +0300

    added modal for localization in header

--- src/frontend/js/createAccount.js
+++ src/frontend/js/createAccount.js
@@ -1,9 +1,9 @@
 import '../createAccount.html';
 import '../scss/header.scss';
 import '../scss/createAccount.scss';
-const togglePassword = document.querySelector('#togglePassword');
-const password = document.querySelector('#PasswordInput_SignUp');
-const confpassword = document.querySelector('#ConfirmPasswordInput');
+const usertogglePassword = document.querySelector('#togglePassword');
+const userpassword = document.querySelector('#PasswordInput_SignUp');
+const userconfpassword = document.querySelector('#ConfirmPasswordInput');
 const adminpassword = document.querySelector('#AdminPasswordInput_SignUp');
 const adminconfpassword = document.querySelector('#AdminConfirmPasswordInput');
 const admintogglePassword = document.querySelector('#AdmintogglePassword');
@@ -23,12 +23,12 @@ userButton.addEventListener('click', function (e) {
     adminBlock.style.display = "none"
 });
 
-togglePassword.addEventListener('click', function (e) {
+usertogglePassword.addEventListener('click', function (e) {
     // toggle the type attribute
-    const type1 = password.getAttribute('type') === 'password' ? 'text' : 'password';
-    const type2 = confpassword.getAttribute('type') === 'password' ? 'text' : 'password';
-    password.setAttribute('type', type1);
-    confpassword.setAttribute('type', type2);
+    const type1 = userpassword.getAttribute('type') === 'password' ? 'text' : 'password';
+    const type2 = userconfpassword.getAttribute('type') === 'password' ? 'text' : 'password';
+    userpassword.setAttribute('type', type1);
+    userconfpassword.setAttribute('type', type2);
     // toggle the eye slash icon
     this.classList.toggle('fa-eye-slash');
 });
--- src/frontend/js/signIn.js
+++ src/frontend/js/signIn.js
@@ -1,3 +1,14 @@
 import '../signIn.html';
 import '../scss/header.scss';
 import '../scss/signIn.scss';
+
+const togglePassword = document.querySelector('#togglePassword');
+const password = document.querySelector('#PasswordInput_SignIn');
+
+togglePassword.addEventListener('click', function (e) {
+    // toggle the type attribute
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+    // toggle the eye slash icon
+    this.classList.toggle('fa-eye-slash');
+});
